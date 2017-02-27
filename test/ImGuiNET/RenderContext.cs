e3460985bfb566e24041599dbde2495964b30474
Eric Tuvesson <eric.tuvesson@gmail.com>
2017-02-27 03:19:12 +0100

    Move test folder

---
+++ test/ImGuiNET/RenderContext.cs
@@ -0,0 +1,249 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+
+namespace ImGuiNET.OpenTK
+{
+    public class RenderContext
+    {
+        public readonly NativeWindow Window;
+
+        private int s_fontTexture;
+        private float wheelPosition;
+
+        public RenderContext(NativeWindow window)
+        {
+            this.Window = window ?? throw new ArgumentNullException(nameof(window));
+
+            window.KeyDown += OnKeyDown;
+            window.KeyUp += OnKeyUp;
+            window.KeyPress += OnKeyPress;
+
+            ImGui.GetIO().FontAtlas.AddDefaultFont();
+
+            SetOpenTKKeyMappings();
+            
+            CreateDeviceObjects();
+        }
+
+        public void BeginFrame(float elapsedTime = 1.0f / 60.0f)
+        {
+            IO io = ImGui.GetIO();
+            io.DisplaySize = new System.Numerics.Vector2(Window.Width, Window.Height);
+            io.DeltaTime = elapsedTime;
+
+            UpdateImGuiInput(io);
+
+            ImGui.NewFrame();
+        }
+
+        public unsafe void EndFrame()
+        {
+            ImGui.Render();
+
+            DrawData* data = ImGui.GetDrawData();
+            RenderImDrawData(data);
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            ImGui.AddInputCharacter(e.KeyChar);
+        }
+
+        private void OnKeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            ImGui.GetIO().KeysDown[(int)e.Key] = true;
+            UpdateModifiers(e);
+        }
+
+        private void OnKeyUp(object sender, KeyboardKeyEventArgs e)
+        {
+            ImGui.GetIO().KeysDown[(int)e.Key] = false;
+            UpdateModifiers(e);
+        }
+
+        private unsafe int OnTextEdited(TextEditCallbackData* data)
+        {
+            char currentEventChar = (char)data->EventChar;
+            return 0;
+        }
+
+        private static void UpdateModifiers(KeyboardKeyEventArgs e)
+        {
+            IO io = ImGui.GetIO();
+            io.AltPressed = e.Alt;
+            io.CtrlPressed = e.Control;
+            io.ShiftPressed = e.Shift;
+        }
+
+        private unsafe void CreateDeviceObjects()
+        {
+            IO io = ImGui.GetIO();
+
+            // Build texture atlas
+            FontTextureData texData = io.FontAtlas.GetTexDataAsAlpha8();
+
+            // Create OpenGL texture
+            s_fontTexture = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, s_fontTexture);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
+            GL.TexImage2D(
+                TextureTarget.Texture2D,
+                0,
+                PixelInternalFormat.Alpha,
+                texData.Width,
+                texData.Height,
+                0,
+                PixelFormat.Alpha,
+                PixelType.UnsignedByte,
+                new IntPtr(texData.Pixels));
+
+            // Store the texture identifier in the ImFontAtlas substructure.
+            io.FontAtlas.SetTexID(s_fontTexture);
+
+            // Cleanup (don't clear the input data if you want to append new fonts later)
+            //io.Fonts->ClearInputData();
+            io.FontAtlas.ClearTexData();
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
+
+        private static void SetOpenTKKeyMappings()
+        {
+            IO io = ImGui.GetIO();
+            io.KeyMap[GuiKey.Tab] = (int)Key.Tab;
+            io.KeyMap[GuiKey.LeftArrow] = (int)Key.Left;
+            io.KeyMap[GuiKey.RightArrow] = (int)Key.Right;
+            io.KeyMap[GuiKey.UpArrow] = (int)Key.Up;
+            io.KeyMap[GuiKey.DownArrow] = (int)Key.Down;
+            io.KeyMap[GuiKey.PageUp] = (int)Key.PageUp;
+            io.KeyMap[GuiKey.PageDown] = (int)Key.PageDown;
+            io.KeyMap[GuiKey.Home] = (int)Key.Home;
+            io.KeyMap[GuiKey.End] = (int)Key.End;
+            io.KeyMap[GuiKey.Delete] = (int)Key.Delete;
+            io.KeyMap[GuiKey.Backspace] = (int)Key.BackSpace;
+            io.KeyMap[GuiKey.Enter] = (int)Key.Enter;
+            io.KeyMap[GuiKey.Escape] = (int)Key.Escape;
+            io.KeyMap[GuiKey.A] = (int)Key.A;
+            io.KeyMap[GuiKey.C] = (int)Key.C;
+            io.KeyMap[GuiKey.V] = (int)Key.V;
+            io.KeyMap[GuiKey.X] = (int)Key.X;
+            io.KeyMap[GuiKey.Y] = (int)Key.Y;
+            io.KeyMap[GuiKey.Z] = (int)Key.Z;
+        }
+
+        private unsafe void UpdateImGuiInput(IO io)
+        {
+            MouseState cursorState = Mouse.GetCursorState();
+            MouseState mouseState = Mouse.GetState();
+
+            if (Window.Bounds.Contains(cursorState.X, cursorState.Y))
+            {
+                Point windowPoint = Window.PointToClient(new Point(cursorState.X, cursorState.Y));
+                io.MousePosition = new System.Numerics.Vector2(windowPoint.X / io.DisplayFramebufferScale.X, windowPoint.Y / io.DisplayFramebufferScale.Y);
+            }
+            else
+            {
+                io.MousePosition = new System.Numerics.Vector2(-1f, -1f);
+            }
+
+            io.MouseDown[0] = mouseState.LeftButton == ButtonState.Pressed;
+            io.MouseDown[1] = mouseState.RightButton == ButtonState.Pressed;
+            io.MouseDown[2] = mouseState.MiddleButton == ButtonState.Pressed;
+
+            float newWheelPos = mouseState.WheelPrecise;
+            io.MouseWheel = newWheelPos - wheelPosition;
+            wheelPosition = newWheelPos;
+        }
+
+        private unsafe void RenderImDrawData(DrawData* draw_data)
+        {
+            // We are using the OpenGL fixed pipeline to make the example code simpler to read!
+            // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, vertex/texcoord/color pointers.
+            int last_texture;
+            GL.GetInteger(GetPName.TextureBinding2D, out last_texture);
+            GL.PushAttrib(AttribMask.EnableBit | AttribMask.ColorBufferBit | AttribMask.TransformBit);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.Disable(EnableCap.CullFace);
+            GL.Disable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.ScissorTest);
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.EnableClientState(ArrayCap.TextureCoordArray);
+            GL.EnableClientState(ArrayCap.ColorArray);
+            GL.Enable(EnableCap.Texture2D);
+
+            GL.UseProgram(0);
+
+            // Handle cases of screen coordinates != from framebuffer coordinates (e.g. retina displays)
+            IO io = ImGui.GetIO();
+            ImGui.ScaleClipRects(draw_data, io.DisplayFramebufferScale);
+
+            // Setup orthographic projection matrix
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.PushMatrix();
+            GL.LoadIdentity();
+            GL.Ortho(
+                0.0f,
+                io.DisplaySize.X / io.DisplayFramebufferScale.X,
+                io.DisplaySize.Y / io.DisplayFramebufferScale.Y,
+                0.0f,
+                -1.0f,
+                1.0f);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.PushMatrix();
+            GL.LoadIdentity();
+
+            // Render command lists
+
+            for (int n = 0; n < draw_data->CmdListsCount; n++)
+            {
+                NativeDrawList* cmd_list = draw_data->CmdLists[n];
+                byte* vtx_buffer = (byte*)cmd_list->VtxBuffer.Data;
+                ushort* idx_buffer = (ushort*)cmd_list->IdxBuffer.Data;
+
+                DrawVert vert0 = *((DrawVert*)vtx_buffer);
+                DrawVert vert1 = *(((DrawVert*)vtx_buffer) + 1);
+                DrawVert vert2 = *(((DrawVert*)vtx_buffer) + 2);
+
+                GL.VertexPointer(2, VertexPointerType.Float, sizeof(DrawVert), new IntPtr(vtx_buffer + DrawVert.PosOffset));
+                GL.TexCoordPointer(2, TexCoordPointerType.Float, sizeof(DrawVert), new IntPtr(vtx_buffer + DrawVert.UVOffset));
+                GL.ColorPointer(4, ColorPointerType.UnsignedByte, sizeof(DrawVert), new IntPtr(vtx_buffer + DrawVert.ColOffset));
+
+                for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
+                {
+                    DrawCmd* pcmd = &(((DrawCmd*)cmd_list->CmdBuffer.Data)[cmd_i]);
+                    if (pcmd->UserCallback != IntPtr.Zero)
+                    {
+                        throw new NotImplementedException();
+                    }
+                    else
+                    {
+                        GL.BindTexture(TextureTarget.Texture2D, pcmd->TextureId.ToInt32());
+                        GL.Scissor(
+                            (int)pcmd->ClipRect.X,
+                            (int)(io.DisplaySize.Y - pcmd->ClipRect.W),
+                            (int)(pcmd->ClipRect.Z - pcmd->ClipRect.X),
+                            (int)(pcmd->ClipRect.W - pcmd->ClipRect.Y));
+                        ushort[] indices = new ushort[pcmd->ElemCount];
+                        for (int i = 0; i < indices.Length; i++) { indices[i] = idx_buffer[i]; }
+                        GL.DrawElements(PrimitiveType.Triangles, (int)pcmd->ElemCount, DrawElementsType.UnsignedShort, new IntPtr(idx_buffer));
+                    }
+                    idx_buffer += pcmd->ElemCount;
+                }
+            }
+
+            // Restore modified state
+            GL.DisableClientState(ArrayCap.ColorArray);
+            GL.DisableClientState(ArrayCap.TextureCoordArray);
+            GL.DisableClientState(ArrayCap.VertexArray);
+            GL.BindTexture(TextureTarget.Texture2D, last_texture);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.PopMatrix();
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.PopMatrix();
+            GL.PopAttrib();
+        }
+    }
+}
