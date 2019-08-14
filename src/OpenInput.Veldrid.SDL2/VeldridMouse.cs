8f3a3fb56ef8778627d267fe2c021110efd03a87
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-08-14 18:15:59 +0200

    Add mouse support

--- src/OpenInput.Veldrid.SDL2/VeldridMouse.cs
+++ src/OpenInput.Veldrid.SDL2/VeldridMouse.cs
@@ -1,10 +1,15 @@
 ﻿namespace OpenInput
 {
     using OpenInput.Trackers;
+    using System.Collections.Generic;
+    using System.Linq;
     using Veldrid;
 
     public class VeldridMouse : VeldridDevice, IMouse
     {
+        private readonly HashSet<MouseButton> pressedButtons = new HashSet<MouseButton>();
+        private MouseState currentState = new MouseState();
+
         /// <inheritdoc />
         public string Name => "Veldrid Mouse";
 
@@ -12,16 +17,13 @@
         public IMouseTracker CreateTracker() => new BasicMouseTracker(this);
 
         /// <inheritdoc />
-        public MouseState GetCurrentState()
-        {
-            // FIXME:
-            return new MouseState();
-        }
+        public MouseState GetCurrentState() => this.currentState;
 
         /// <inheritdoc />
         public void GetPosition(out int x, out int y)
         {
-            throw new System.NotImplementedException();
+            x = this.currentState.X;
+            y = this.currentState.Y;
         }
 
         /// <inheritdoc />
@@ -32,7 +34,27 @@
 
         internal override void UpdateSnapshot(InputSnapshot snapshot)
         {
+            foreach (var key in snapshot.MouseEvents)
+            {
+                if (key.Down)
+                {
+                    this.pressedButtons.Add(key.MouseButton);
+                }
+                else
+                {
+                    this.pressedButtons.Remove(key.MouseButton);
+                }
+            }
 
+            this.currentState = new MouseState(
+                (int)snapshot.MousePosition.X,
+                (int)snapshot.MousePosition.Y,
+                (int)snapshot.WheelDelta,
+                pressedButtons.Contains(MouseButton.Left),
+                pressedButtons.Contains(MouseButton.Middle),
+                pressedButtons.Contains(MouseButton.Right),
+                pressedButtons.Contains(MouseButton.Button1),
+                pressedButtons.Contains(MouseButton.Button2));
         }
     }
 }
