edd68c396cd65b429351222ac377c943c4ee0492
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-08-25 13:12:46 +0200

    Update main workflow

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,13 +1,13 @@
 :: Add NuGet Source
 echo Add NuGet Source
-./nuget.exe source Add -Name "GitHub - erictuvesson" -Source "https://nuget.pkg.github.com/erictuvesson/index.json" -UserName PACKAGE_REGISTRY_USERNAME -Password PACKAGE_REGISTRY_PASSWORD
+nuget.exe source Add -Name "GitHub - erictuvesson" -Source "https://nuget.pkg.github.com/erictuvesson/index.json" -UserName PACKAGE_REGISTRY_USERNAME -Password PACKAGE_REGISTRY_PASSWORD
 echo Added NuGet Source
 
 :: Publish Packages
 echo Publish Packages
 for %%f in (../release/*.nupkg) do (
   echo Publish Package %%~nf
-  ./nuget.exe push %%~nf -Source "GitHub - erictuvesson"
+  nuget.exe push %%~nf -Source "GitHub - erictuvesson"
 )
 echo Finished Publishing Packages
 
@@ -15,6 +15,6 @@ echo Finished Publishing Packages
 echo Publish Package Symbols
 for %%f in (../release/*.snupkg) do (
   echo Publish Symbol %%~nf
-  ./nuget.exe push %%~nf -Source "GitHub - erictuvesson"
+  nuget.exe push %%~nf -Source "GitHub - erictuvesson"
 )
 echo Finished Publishing Package Symbols
