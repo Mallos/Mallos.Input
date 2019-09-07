9b017f6b6bc7938087e99c2138e00086b719b974
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-09-07 14:05:20 +0200

    Update build-packages.cmd

--- build/build-packages.cmd
+++ build/build-packages.cmd
@@ -6,4 +6,3 @@ dotnet build -c Release ..\Mallos.Input.sln
 
 dotnet pack -c Release ..\src\Mallos.Input\Mallos.Input.csproj
 dotnet pack -c Release ..\src\Mallos.Input.Veldrid.SDL2\Mallos.Input.Veldrid.SDL2.csproj
-dotnet pack -c Release ..\src\Mallos.Input.Debug.ImGuiNET\Mallos.Input.Debug.ImGuiNET.csproj
