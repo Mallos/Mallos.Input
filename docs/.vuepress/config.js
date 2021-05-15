364e629a7c11d8ad3389adfc4ce52fa74213651a
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-05-15 02:48:45 +0200

    docs: update base url path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
-  base: 'Mallos.Input',
+  base: '/Mallos.Input/',
 
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
