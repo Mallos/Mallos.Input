a39e3fccc21b97bf79df09a36db5d40d9b8a5fff
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-05-15 02:01:54 +0200

    ci: update workflows

--- build/build-packages.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-@setlocal
-
-dotnet restore ..\Mallos.Input.sln
-dotnet build -c Release ..\Mallos.Input.sln
-
-dotnet pack -c Release ..\src\Mallos.Input\Mallos.Input.csproj
-dotnet pack -c Release ..\src\Mallos.Input.Veldrid.SDL2\Mallos.Input.Veldrid.SDL2.csproj
